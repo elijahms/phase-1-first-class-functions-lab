5723e41c858d3025e5b47c77b4b190d29e1b858f
Elijah Silverman <elijahmsilverman@gmail.com>
2021-10-13 10:59:31 -0400

    commit tree 3ea86decd923b34babd73db89960a01ae03654ab

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
-// Code your solution in this file!
+//Code your solution in this file!
+
+const returnFirstTwoDrivers = function(array) {
+    const newArray = [array[0], array[1]]
+    return newArray
+}
+const returnLastTwoDrivers = function(lastArray) {
+    const lastNewArray = [lastArray[lastArray.length -2], lastArray[lastArray.length -1]]
+    return lastNewArray
+}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]
+
+function createFareMultiplier(multiplier) {
+    return function(fare) {
+        return multiplier * fare
+    }
+}
+
+const fareDoubler = function(fare) {
+    return createFareMultiplier(2)(fare)
+}
+
+const fareTripler = function(fare) {
+    return createFareMultiplier(3)(fare)
+}
+
+function selectDifferentDrivers(arrayOfDrivers, secondthang = '') {
+    if (secondthang === returnFirstTwoDrivers) {
+        return returnFirstTwoDrivers(arrayOfDrivers)
+    } else {
+        return returnLastTwoDrivers(arrayOfDrivers)
+    }
+}
